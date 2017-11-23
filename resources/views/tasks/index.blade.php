57a80bd5c9d20818168317c8ddd6981adb12bf25
alwizo <alwizo.com@gmail.com>
2017-11-23 11:38:42 +0100

    Created a task form

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,25 @@
 @extends ('layouts.app')
 
 @section('content')
-    Task index
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <form action="{{ url('task') }}" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <div class="row">
+                        <label for="name" class="offset-sm-3 col-sm-1 control-label">Task</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="name" id="name" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="offset-sm-4 col-sm-3">
+                            <button type="submit" class="btn btn-success">Add task</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
