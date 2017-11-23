1159c0c581b9b6324256cc9b9d23fba32e48bf42
alwizo <alwizo.com@gmail.com>
2017-11-23 12:03:08 +0100

    Added the task validation

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="panel panel-default">
         <div class="panel-body">
+            @include('common.errors')
             <form action="{{ url('task') }}" method="post" class="form-horizontal">
                 <div class="form-group">
                     <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,8 +17,16 @@ Route::get('/', function () {
 	return view('tasks.index');
 });
 
-Route::post('/task', function (){
+Route::post('/task', function (Request $request){
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+	]);
 
+	if($validator->fails()){
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
 });
 
 Route::delete('/task/{task}', function (){
